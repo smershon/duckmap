daf70610350c0398181c81bf62a0d7c030ace465
stuart <stuart@spotify.com>
2016-05-06 14:31:43 -0700

    basic vertex buffer and index metching

--- duckmap/static/js/duckmap.js
+++ duckmap/static/js/duckmap.js
@@ -1,9 +1,28 @@
-$(function() {
+var DuckMap = function(cfg) {
+    this.canvas = cfg.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.vertex_buffer = cfg.buffer; 
+    this.polygons = cfg.polygons;
+    this.dataset = cfg.dataset;
+    this.key = cfg.key;
+}
+
+DuckMap.prototype = {
     
-  var map = $(".duckmap")[0];
-  var ctx = map.getContext("2d");
-  ctx.moveTo(0,0);
-  ctx.lineTo(200,100);
-  ctx.stroke();
+    "redraw": function() {
+        for (var shapename in this.polygons) {
+            var shape = this.polygons[shapename];
+            this.ctx.fillStyle = this.key[this.dataset[shapename]];
+            console.log(shapename, this.dataset[shapename], this.key[this.dataset[shapename]]);
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.vertex_buffer[shape[0]], this.vertex_buffer[shape[0] + 1]);
+            for (var i = 1; i < shape.length; i++) {
+                this.ctx.lineTo(this.vertex_buffer[shape[i]], this.vertex_buffer[shape[i] + 1]);
+            }
+            this.ctx.closePath();
+            this.ctx.stroke();
+            this.ctx.fill()
+        }
+    }
     
-})
+}
---
+++ duckmap/static/js/sample.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    var buffer = [10.0, 10.0, 100.0, 150.0, 25.0, 170.0, 125.0, 15.0];
+    var polygons = {
+        "sample1": [0, 1, 2],
+        "sample2": [0, 2, 3]
+    };
+    var dataset = {
+        "sample1": "toast",
+        "sample2": "biscuits"
+    };
+    var key = {
+        "toast": "#ffcf7f",
+        "biscuits": "#ffff7f",
+        "default": "#7f7f7f"
+    }
+
+    var map = new DuckMap({
+        "canvas": $(".duckmap")[0], 
+        "buffer": buffer, 
+        "polygons": polygons,
+        "dataset": dataset,
+        "key": key
+    });
+    map.redraw();
+ 
+    
+})
