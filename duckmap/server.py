faa421007f906dec55572036d463795c40e55477
stuart <stuart@spotify.com>
2016-05-23 18:08:26 -0700

    some more stuff around saving the builder work

--- duckmap/server.py
+++ duckmap/server.py
@@ -3,6 +3,14 @@ from flask import jsonify
 from flask import render_template
 from flask import request
 
+import simplejson as json
+import os
+
+import logging
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__)
 
 with open('/etc/duckmap/google_maps.key', 'rb') as f:
@@ -14,11 +22,19 @@ def sample():
     
 @app.route('/builder/<mapname>')
 def builder(mapname):
-    return render_template('builder.html', map_name=mapname, api_key=api_key)
+    data = json.dumps({'mapname': mapname, 'borders': [], 'areas': {}})
+
+    if (os.path.exists('data/%s.json' % mapname)):
+        with open('data/%s.json' % mapname, 'rb') as f:
+            data = f.readlines()[0].strip()
+
+    return render_template('builder.html', map_name=mapname, api_key=api_key, data=data)
 
 @app.route('/builder/savemap', methods=['POST'])
 def savemap():
     data = request.json
+    with open('data/%s.json' % data['mapname'], 'wb') as f:
+        f.write(json.dumps(data));
     return jsonify({'result': 'saved'})
 
 if __name__ == '__main__':
--- duckmap/static/js/builder.js
+++ duckmap/static/js/builder.js
@@ -9,6 +9,9 @@ function initMap() {
     
     var myLatlng = {lat: 45.0, lng: -121.044};
     
+    var map_data = JSON.parse($("#data_json").html());
+    console.log(map_data);
+    
     var segments = [];
     var areas = [];
     var area_borders = [];
@@ -29,78 +32,102 @@ function initMap() {
         </select>`;
         
     var area_html = `<input type="text"></input>`
-    
-    var poly = new google.maps.Polyline({
-        path: data,
-        editable: true,
-        geodesic: true,
-        strokeColor: "#ffcf3f",
-        strokeOpacity: 0.8,
-        strokeWeight: 2
-    });
 
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 6,
         center: myLatlng
     });
     
-    google.maps.event.addDomListener(document, "keypress", function (e) {
-
-        if (e.keyCode == 115) {
-            // 's'
-            if (drawing) {
-                segments.push(currentSegment);
-                var idx = segments.length - 1;
-                $("#border_info tbody").append("<tr id=\"border_" + idx + "\"><td>" + 
-                        segments.length + "</td><td>" + type_html + "</td></tr>")
-                $("#border_info tbody select").unbind("change");
-                $("#border_info tbody select").change(function() {
-                    var uid = $(this).parent().parent().attr("id");
-                    var idx = parseInt(uid.split("_").pop());
-                    segments[idx].setOptions(borderStyles[$(this).val()]);
-                });
-                currentSegment = null;
-                drawing = false;
-            } else if (building) {
-                areas.push(currentArea);
-                area_borders.push(currentAreaBorders);
-                var idx = areas.length - 1;
-                $("#area_info tbody").append("<tr id=\"area_" + idx + "\"><td>" + 
-                        areas.length + "</td><td>" + area_html + 
-                        "</td><td><button class=\"show\">hide</button></td></tr>");
-                $("#area_info .show").unbind("click");
-                $("#area_info .show").click(function() {
-                    var uid = $(this).parent().parent().attr("id");
-                    var idx = parseInt(uid.split("_").pop());
-                    if ($(this).html() == "hide") {
-                        areas[idx].setMap(null);
-                        $(this).html("show");
-                    } else {
-                        areas[idx].setMap(map);
-                        $(this).html("hide");
-                    }
-                });
-                currentArea = null;
-                currentAreaBorders = null;
-                building = false;
-            } 
-        }
+    map_data['borders'].forEach(function(b) {
+        b['points'].forEach(function(v) {
+            pushVertex(new google.maps.LatLng(v));
+        });
+        saveCurrentBorder();
     });
     
-    map.setOptions({draggableCursor:"crosshair"});
-
-    map.addListener("click", function(e) {
-        pushEdge(e);
+    map_data['areas'].forEach(function(a) {
+        a['borders'].forEach(function(b) {
+            pushBorder(segments[b]);
+        });
+        saveCurrentArea();
     });
     
-    function vertexMatch(v0, v1) {
-        return (v0.lat() == v1.lat() && v0.lng() == v1.lng()); 
+    function saveCurrentBorder() {
+        segments.push(currentSegment);
+        var idx = segments.length - 1;
+        $("#border_info tbody").append("<tr id=\"border_" + idx + "\"><td>" + 
+                segments.length + "</td><td>" + type_html + "</td></tr>")
+        $("#border_info tbody select").unbind("change");
+        $("#border_info tbody select").change(function() {
+            var uid = $(this).parent().parent().attr("id");
+            var idx = parseInt(uid.split("_").pop());
+            segments[idx].setOptions(borderStyles[$(this).val()]);
+        });
+        currentSegment = null;
+        drawing = false;        
     }
     
-    function pushEdge(e) {
+    function saveCurrentArea() {
+        areas.push(currentArea);
+        area_borders.push(currentAreaBorders);
+        var idx = areas.length - 1;
+        $("#area_info tbody").append("<tr id=\"area_" + idx + "\"><td>" + 
+                areas.length + "</td><td>" + area_html + 
+                "</td><td><button class=\"show\">hide</button></td></tr>");
+        $("#area_info .show").unbind("click");
+        $("#area_info .show").click(function() {
+            var uid = $(this).parent().parent().attr("id");
+            var idx = parseInt(uid.split("_").pop());
+            if ($(this).html() == "hide") {
+                areas[idx].setMap(null);
+                $(this).html("show");
+            } else {
+                areas[idx].setMap(map);
+                $(this).html("hide");
+            }
+        });
+        currentArea = null;
+        currentAreaBorders = null;
+        building = false;        
+    }
+    
+    function pushBorder(border) {
+        // Push a border onto the area currently being built
+        if (!building) {
+            building = true;
+            currentArea = new google.maps.Polygon({
+                path: [],
+                strokeColor: '#FFFFFF',
+                strokeOpacity: 1.0,
+                strokeWeight: 0,
+                fillColor: '#FFFF00',
+                fillOpacity: 0.4,
+                zIndex: -1                        
+            });
+            currentAreaBorders = [];
+            currentArea.setMap(map);
+        }
+        currentAreaBorders.push(segments.indexOf(border));
+        var existingPath = currentArea.getPath();
+        var endPoint = existingPath.getAt(existingPath.getLength() - 1);
+        if (existingPath.getLength() <= 0 || vertexMatch(endPoint, border.getPath().getAt(0))) {
+            border.getPath().forEach(function(ll) {
+                currentArea.getPath().push(ll);
+            });
+        } else {
+            // Reverse the order of this.getPath() before pushing
+            var newPath = border.getPath();
+            for (var i = newPath.getLength() - 1; i--; i >= 0) {
+                currentArea.getPath().push(newPath.getAt(i));
+            }
+        }        
+    }
+    
+    function pushVertex(vertex) {
+        // Push a vertex onto the border currently being built
         if (drawing) {
             // Add a vertex to the current segment
-            currentSegment.getPath().push(e.latLng);
+            currentSegment.getPath().push(vertex);
         } else {
             // Place a marker and start a new segment
             currentSegment = new google.maps.Polyline({
@@ -116,41 +143,36 @@ function initMap() {
                 if (e.vertex == undefined) {
                     highlightBorder(this);
                 } else {
-                    pushEdge(e);
+                    pushVertex(e.latLng);
                 }
             });
-            google.maps.event.addListener(currentSegment, "rightclick", function(e) {
-                if (!building) {
-                    building = true;
-                    currentArea = new google.maps.Polygon({
-                        path: [],
-                        strokeColor: '#FFFFFF',
-                        strokeOpacity: 1.0,
-                        strokeWeight: 0,
-                        fillColor: '#FFFF00',
-                        fillOpacity: 0.4,
-                        zIndex: -1                        
-                    });
-                    currentAreaBorders = [];
-                    currentArea.setMap(map);
-                }
-                currentAreaBorders.push(segments.indexOf(this));
-                var existingPath = currentArea.getPath();
-                var endPoint = existingPath.getAt(existingPath.getLength() - 1);
-                if (existingPath.getLength() <= 0 || vertexMatch(endPoint, this.getPath().getAt(0))) {
-                    this.getPath().forEach(function(ll) {
-                        currentArea.getPath().push(ll);
-                    });
-                } else {
-                    // Reverse the order of this.getPath() before pushing
-                    var newPath = this.getPath();
-                    for (var i = newPath.getLength() - 1; i--; i >= 0) {
-                        currentArea.getPath().push(newPath.getAt(i));
-                    }
-                }
+            google.maps.event.addListener(currentSegment, "rightclick", function() {
+                pushBorder(this);
             });
-            currentSegment.getPath().push(e.latLng);
+            currentSegment.getPath().push(vertex);
+        }        
+    }
+    
+    google.maps.event.addDomListener(document, "keypress", function (e) {
+
+        if (e.keyCode == 115) {
+            // 's'
+            if (drawing) {
+                saveCurrentBorder();
+            } else if (building) {
+                saveCurrentArea();
+            } 
         }
+    });
+    
+    map.setOptions({draggableCursor:"crosshair"});
+
+    map.addListener("click", function(e) {
+        pushVertex(e.latLng);
+    });
+    
+    function vertexMatch(v0, v1) {
+        return (v0.lat() == v1.lat() && v0.lng() == v1.lng()); 
     }
     
     function highlightBorder(border) {
@@ -165,7 +187,7 @@ function initMap() {
     $(".save_map").click(function() {
         var map_name = $("#map_name").html();
         var flat_borders = [];
-        var flat_areas = {};
+        var flat_areas = [];
         
         $("#border_info tbody tr").each(function(i, row) {
             var border = segments[i];
@@ -185,11 +207,10 @@ function initMap() {
         
         $("#area_info tbody tr").each(function(i, row) {
             var area_name = $(row).find("input").val();
-            if (flat_areas[area_name]) {
-                flat_areas[area_name].push(area_borders[i]);
-            } else {
-                flat_areas[area_name] = [area_borders[i]];
-            }
+            flat_areas.push({
+                name: area_name,
+                borders: area_borders[i]
+            });
         });
         
         var data = {
